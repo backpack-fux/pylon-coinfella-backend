015d914f4585517e5dc53446420ce3b8b478d034
pei <peipeilong116@gmail.com>
2023-11-19 21:27:22 -0500

    build 1.1

--- dist/utils/dinero.js
+++ dist/utils/dinero.js
@@ -1,26 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertAnyDineroPropertiesToAmount = exports.newDineroDollars = exports.newDinero = exports.isInstanceOfDinero = void 0;
-const dinero_js_1 = __importDefault(require("dinero.js"));
+const Dinero = require("dinero.js");
 const isInstanceOfDinero = (money) => {
     // Lets assume that money is Dinero instance if the money has getAmount and getCurrency property
     return !!(money && money.getAmount && money.getCurrency);
 };
 exports.isInstanceOfDinero = isInstanceOfDinero;
-function newDinero(amountInCents, currency = 'USD') {
+function newDinero(amountInCents, currency = "USD") {
     if (isNaN(amountInCents)) {
-        throw new Error('amount should be a number');
+        throw new Error("amount should be a number");
     }
     // amount is comming as cent, so we round it to integer
     const rounderAmountInCents = Math.round(amountInCents);
     // @ts-ignore
-    return (0, dinero_js_1.default)({ amount: rounderAmountInCents, currency });
+    return Dinero({ amount: rounderAmountInCents, currency });
 }
 exports.newDinero = newDinero;
-function newDineroDollars(amountInDollars, currency = 'USD') {
+function newDineroDollars(amountInDollars, currency = "USD") {
     return newDinero(amountInDollars * 100, currency);
 }
 exports.newDineroDollars = newDineroDollars;
