1830d05753a5c8b67e6ab2eeb11a759737e6069f
pei <peipeilong116@gmail.com>
2023-11-20 22:53:27 -0500

    chore: add bootstraps

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,25 @@
+import "../src/models";
 import express, { Request, Response } from "express";
+require("dotenv").config();
+import models from "../src/models";
+import cors from "cors";
+import { resError } from "../src/middleware/resError";
+import { initAuth } from "../src/auth";
+
+const { sequelize } = models;
 
 const bootstrap = async () => {
   const app = express();
+
+  await sequelize.authenticate();
+
   app.get("/", (_req: Request, res: Response) => {
-    return res.send("Express Typescript on Vercel" + process.env.NODE_ENV);
+    return res.send("Coinfella api");
   });
 
-  app.get("/ping", (_req: Request, res: Response) => {
-    return res.send("pong 🏓");
-  });
+  app.use(cors());
+  app.use(resError);
+  initAuth(app);
 
   return app;
 };
