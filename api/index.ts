947fbdb76c274ecc59eb009ddbb5a7e961b87922
pei <peipeilong116@gmail.com>
2023-11-21 00:40:37 -0500

    chore: disable imports

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,19 @@
 import express, { Request, Response } from "express";
-require("dotenv").config();
-import cors from "cors";
-import models from "../src/models";
-import { resError } from "../src/middleware/resError";
-import { initAuth } from "../src/auth";
-import { initGraphql } from "../src/graphql";
-import bodyParser from "body-parser";
-import { initRoutes } from "../src/routes";
+// require("dotenv").config();
+// import cors from "cors";
+// import models from "../src/models";
+// import { resError } from "../src/middleware/resError";
+// import { initAuth } from "../src/auth";
+// import { initGraphql } from "../src/graphql";
+// import bodyParser from "body-parser";
+// import { initRoutes } from "../src/routes";
 
-const { sequelize } = models;
+// const { sequelize } = models;
 
 const bootstrap = async () => {
   const app = express();
 
-  await sequelize.authenticate();
+  // await sequelize.authenticate();
 
   app.get("/", (_req: Request, res: Response) => {
     return res.send("Coinfella api" + process.env.NODE_ENV);
@@ -25,14 +25,14 @@ const bootstrap = async () => {
     });
   });
 
-  app.use(cors());
-  app.use(resError);
-  app.use(bodyParser.json());
-  // initAuth(app);
-  initRoutes(app);
-  await initGraphql(app);
+  // app.use(cors());
+  // app.use(resError);
+  // app.use(bodyParser.json());
+  // // initAuth(app);
+  // initRoutes(app);
+  // await initGraphql(app);
 
-  await initGraphql(app);
+  // await initGraphql(app);
 
   return app;
 };
