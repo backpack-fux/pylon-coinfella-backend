3cf4d307e0d51855ccf3e357d1e0d9774f9e82fd
pei <peipeilong116@gmail.com>
2023-11-20 22:59:06 -0500

    chore: update config

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,9 @@
-import "../src/models";
 import express, { Request, Response } from "express";
 require("dotenv").config();
-import models from "../src/models";
 import cors from "cors";
 import { resError } from "../src/middleware/resError";
 import { initAuth } from "../src/auth";
 import { initGraphql } from "../src/graphql";
-
-const { sequelize } = models;
 
 const bootstrap = async () => {
   const app = express();
