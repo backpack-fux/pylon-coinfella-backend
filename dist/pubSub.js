1111939dac922f16443b30c0b2d0808f8552c2ef
pei <peipeilong116@gmail.com>
2023-11-20 22:49:32 -0500

    chore: add public

--- dist/pubSub.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_subscriptions_1 = require("graphql-subscriptions");
-const typedi_1 = __importDefault(require("typedi"));
-typedi_1.default.set("pubsub", new graphql_subscriptions_1.PubSub());
-//# sourceMappingURL=pubSub.js.map
