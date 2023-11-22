026dbcb4b7d6f048c67800a5e91bed34c3245879
pei <peipeilong116@gmail.com>
2023-11-21 22:48:11 -0500

    chore: add partner user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,11 @@ import * as express from "express";
 import job from "./job";
 import user from "./user";
 import partner from "./v2/partner";
+import partnerUser from "./v2/user";
+
 export const initRoutes = async (app: express.Application) => {
   app.use("/", job);
   app.use("/", user);
   app.use("/", partner);
+  app.use("/", partnerUser);
 };
