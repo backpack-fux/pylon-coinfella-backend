2b74035cac3e2709387178d1a791daba7ca3478f
pei <peipeilong116@gmail.com>
2023-12-22 10:24:36 -0500

    feat: processing aft

--- src/services/checkoutSdk.ts
+++ src/services/checkoutSdk.ts
@@ -1,26 +1,26 @@
-import { Checkout as CheckoutSdk } from 'checkout-sdk-node';
-import { Config } from '../config';
-import { Checkout } from '../models/Checkout';
-import { log } from '../utils';
+import { Checkout as CheckoutSdk } from "checkout-sdk-node";
+import { Config } from "../config";
+import { Checkout } from "../models/Checkout";
+import { log } from "../utils";
 
 const cko = new CheckoutSdk(Config.checkoutSecureKey, {
   pk: Config.checkoutPublicKey,
-  environment: Config.isProduction ? "production" : "sandbox"
+  environment: Config.isProduction ? "production" : "sandbox",
 });
 
 export class CheckoutSdkService {
   static getInstance() {
-    return new CheckoutSdkService()
+    return new CheckoutSdkService();
   }
 
   async charge(checkout: Checkout) {
     try {
-      const checkoutRequest = await checkout.getCheckoutRequest()
+      const checkoutRequest = await checkout.getCheckoutRequest();
       const partner = await checkoutRequest?.getPartner();
-    
+
       const res = await cko.payments.request({
         source: {
-          type: 'token',
+          type: "token",
           token: checkout.checkoutTokenId,
           billing_address: {
             address_line1: checkout.streetAddress,
@@ -28,12 +28,39 @@ export class CheckoutSdkService {
             city: checkout.city,
             state: checkout.state,
             postalCode: checkout.postalCode,
-            country: 'US',
+            country: "US",
+          },
+        },
+        processing: {
+          aft: true,
+        },
+        recipient: {
+          first_name: checkout.firstName,
+          last_name: checkout.lastName,
+          address: {
+            address_line1: checkout.streetAddress,
+            address_line2: checkout.streetAddress2,
+            city: checkout.city,
+            state: checkout.state,
+            postalCode: checkout.postalCode,
+            country: "US",
+          },
+        },
+        sender: {
+          first_name: checkout.firstName,
+          last_name: checkout.lastName,
+          address: {
+            address_line1: checkout.streetAddress,
+            address_line2: checkout.streetAddress2,
+            city: checkout.city,
+            state: checkout.state,
+            postalCode: checkout.postalCode,
+            country: "US",
           },
         },
         currency: checkout.totalChargeAmountMoney.getCurrency(),
         amount: checkout.totalChargeAmountMoney.getAmount(),
-        payment_type: 'Regular',
+        payment_type: "Regular",
         reference: partner?.id || `ORDER ${checkout.id}`,
         description: `Purchase USDC for $${checkout.amount}`,
         processing_channel_id: Config.checkoutProcessingChannelId,
@@ -43,18 +70,21 @@ export class CheckoutSdkService {
         },
         metadata: {
           value: `Purchase USDC for $${checkout.amount}`,
-          checkoutId: checkout.id
+          checkoutId: checkout.id,
         },
-      })
+      });
 
-      return res as any
+      return res as any;
     } catch (err) {
-      log.warn({
-        func: 'CheckoutService.charge',
-        checkoutId: checkout.id,
-        err
-      }, 'Failed charge from checkout.com')
-      throw err
+      log.warn(
+        {
+          func: "CheckoutService.charge",
+          checkoutId: checkout.id,
+          err,
+        },
+        "Failed charge from checkout.com"
+      );
+      throw err;
     }
   }
 }
