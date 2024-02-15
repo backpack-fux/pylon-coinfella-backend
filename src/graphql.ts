2a28ec9f2b57b2606a8bfa602fa50782069595e9
thomas779 <thomasehumphreys@gmail.com>
2024-02-15 13:14:19 -0300

    chore: rm unused files

--- src/graphql.ts
+++ src/graphql.ts
@@ -3,15 +3,11 @@ import { NonEmptyArray, buildSchema } from "type-graphql";
 import { Container } from "typedi";
 import * as _ from "lodash";
 
-import { Config } from "./config";
 import { Express } from "express";
 import cors from "cors";
 import { CheckoutResolver } from "./resolvers/checkout.resolver";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import type http from "http";
 import { authMiddlewareForGraphql } from "./middleware/auth";
 import { customAuthChecker } from "./auth/authChecker";
-import path from "path";
 import { UserResolver } from "./resolvers/user.resolver";
 import { CheckoutRequestResolver } from "./resolvers/checkoutRequest.resolver";
 
